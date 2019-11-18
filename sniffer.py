013e105851d2a8824f0f80266e8306effee2d362
des2048 <electrochange@mail.ru>
2019-11-18 21:48:51 +0300

    main() procedure as entry point

--- sniffer.py
+++ sniffer.py
@@ -11,4 +11,19 @@ def get_mac_addr(mac_data):
 def ethernet_frame(data):
     dst_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[0:14])
 
-    return get_mac_addr(dst_mac), get_mac_addr(src_mac), data[14:]
+    return get_mac_addr(dst_mac), get_mac_addr(src_mac), proto, data[14:]
+
+
+def main():
+    sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(3))
+
+    while True:
+        raw_data, addr = sock.recvfrom(65536)
+        dst_mac, src_mac, proto, data = ethernet_frame(raw_data)
+        print('src:{}, dst:{}, proto:{}'.format(dst_mac, src_mac, proto))
+
+
+if __name__ == '__main__':
+    main()
+
+
